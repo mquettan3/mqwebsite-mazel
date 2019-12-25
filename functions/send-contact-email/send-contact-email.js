5fa6cf79599314d465c3fb9e583b0ea14f2b25f5
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2019-12-25 15:22:42 -0500

    Updating send-contact-email.js function to send using mailgun instead of direct without SMTP.

--- functions/send-contact-email/send-contact-email.js
+++ functions/send-contact-email/send-contact-email.js
@@ -1,13 +1,16 @@
 // with thanks to https://github.com/Urigo/graphql-modules/blob/8cb2fd7d9938a856f83e4eee2081384533771904/website/lambda/contact.js
-const sendMail = require('sendmail')()
+
+const { MAILGUN_API_KEY, MAILGUN_DOMAIN, FROM_EMAIL_ADDRESS, CONTACT_TO_EMAIL_ADDRESS } = process.env
+const mailgun = require('mailgun-js')({ apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN })
+
 const { validateEmail, validateLength } = require('./validations')
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
   if (!process.env.CONTACT_EMAIL) {
-    return callback(null, {
+    return {
       statusCode: 500,
       body: 'process.env.CONTACT_EMAIL must be defined'
-    })
+    }
   }
 
   const body = JSON.parse(event.body)
@@ -16,50 +19,45 @@ exports.handler = (event, context, callback) => {
     validateLength('body.name', body.name, 3, 50)
   } catch (e) {
     console.log("Invalid Name!" + body.name);
-    return callback(null, {
+    return {
       statusCode: 403,
       body: e.message
-    })
+    }
   }
 
   try {
     validateEmail('body.email', body.email)
   } catch (e) {
     console.log("Invalid Email!" + body.email);
-    return callback(null, {
+    return {
       statusCode: 403,
       body: e.message
-    })
+    }
   }
 
   try {
     validateLength('body.content', body.content, 10, 1000)
   } catch (e) {
     console.log("Invalid content" + body.content)
-    return callback(null, {
+    return {
       statusCode: 403,
       body: e.message
-    })
+    }
   }
-
-  const descriptor = {
-    from: `"${body.email}" <no-reply@marcusquettan.com>`,
-    to: process.env.CONTACT_EMAIL,
+  
+  const mailgunData = {
+    from: FROM_EMAIL_ADDRESS,
+    to: CONTACT_TO_EMAIL_ADDRESS,
+    'h:Reply-To': body.email,
     subject: `${body.name} sent you a message from marcusquettan.com : ${body.subject}`,
     text: body.content
   }
 
-  sendMail(descriptor, e => {
-    if (e) {
-      callback(null, {
-        statusCode: 500,
-        body: e.message
-      })
-    } else {
-      callback(null, {
-        statusCode: 200,
-        body: ''
-      })
-    }
-  })
+  return mailgun.messages().send(mailgunData).then(() => ({
+    statusCode: 200,
+    body: `Successfully sent email from ${body.email} to ${CONTACT_TO_EMAIL_ADDRESS}`
+  })).catch(error => ({
+    statusCode: 422,
+    body: `Failed to send email from ${body.email} to ${CONTACT_TO_EMAIL_ADDRESS}`
+  }));
 }
